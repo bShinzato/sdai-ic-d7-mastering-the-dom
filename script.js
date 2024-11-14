4d4126bc8ea64fdfb9097a138a590b1fcae958a4
Brandon Shinzato <131624991+bShinzato@users.noreply.github.com>
2024-11-13 20:31:43 -0800

    finished

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-// Write your JavaScipt code below this line. 
+// Write your JavaScipt code below this line.
+
+const mainTitle = document.getElementById("main-title");
+mainTitle.textContent = "Welcome to Our Cat Adoption Center";
+
+const description = document.getElementById("description");
+description.textContent = "Find your purrfect companion!";
+
+const cat1Button = document
+  .getElementById("cat-1-button")
+  .addEventListener("click", () => {
+    const whisk = document.getElementById("cat-1");
+    alert("You've adopted your new Friend!!!");
+    whisk.remove();
+  });
+
+  const cat2Button = document
+  .getElementById("cat-2-button")
+  .addEventListener("click", () => {
+    const daisy = document.getElementById("cat-2");
+    alert("You've adopted your new Friend!!!");
+    daisy.remove();
+  });
+
+  const cat3Button = document
+  .getElementById("cat-3-button")
+  .addEventListener("click", () => {
+    const terry = document.getElementById("cat-3");
+    alert("You've adopted your new Friend!!!");
+    terry.remove();
+  });
+
